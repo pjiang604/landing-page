c2e0e55f9d03c80fe28de205b04618282d806aec
pjiang604 <patriciaejiang@gmail.com>
2024-02-23 14:03:30 -0800

    Updated favicon and Head tag

--- src/components/Navigation/HeaderNav/headerNav.tsx
+++ src/components/Navigation/HeaderNav/headerNav.tsx
@@ -4,36 +4,43 @@ import Link from 'next/link'
 import GetStartedBtn from '@/components/Buttons/GetStarted/getStartedBtn'
 import { GhostNavbar } from "react-hamburger-menus";
 import "react-hamburger-menus/dist/style.css";
+import Head from 'next/head';
 
 
 export default function HeaderNav() {
     return (
-        <header className={`p-pagePadding ${styles.header}`}>
-            <div className={styles.logoContainer}>
-                <Logo />
-            </div>
-            <div className={styles.linksContainer}>
-                <GhostNavbar
-                    floatButtonX={5}
-                    floatButtonY={3}
-                    fontColor='var(--black)'
-                    styles={{
-                        navigationBackground: {
-                            backgroundColor: 'var(--white)'
-                        }
-                    }}
-                >
-                    <ul>
-                        <li><Link href={'/'}><h4>Pricing</h4></Link></li>
+        <>
+            <Head>
+                <title>Amplify | Your Canadian Music</title>
+            </Head>
+            <header className={`p-pagePadding ${styles.header}`}>
+                <div className={styles.logoContainer}>
+                    <Logo />
+                </div>
+                <div className={styles.linksContainer}>
+                    <GhostNavbar
+                        floatButtonX={5}
+                        floatButtonY={3}
+                        fontColor='var(--black)'
+                        styles={{
+                            navigationBackground: {
+                                backgroundColor: 'var(--white)'
+                            }
+                        }}
+                    >
+                        <ul>
+                            <li><Link href={'/'}><h4>Pricing</h4></Link></li>
 
-                        <li><Link href={'/'}><h4>About</h4></Link></li>
+                            <li><Link href={'/'}><h4>About</h4></Link></li>
 
-                        <li><Link href={'/'} className={styles.getStartedLink}>Get Started</Link></li>
-                    </ul>
-                </GhostNavbar>
+                            <li><Link href={'/'} className={styles.getStartedLink}>Get Started</Link></li>
+                        </ul>
+                    </GhostNavbar>
 
-            </div>
+                </div>
 
-        </header>
+            </header>
+        </>
+
     )
 }
